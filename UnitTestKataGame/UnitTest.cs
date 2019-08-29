a74d433351b7cd3ff9b6d2dd36932bdc2e042f95
Deyvi Josue Ortiz Vidaurre <DORTIZV@farmacorp.com>
2019-08-28 22:43:31 -0400

    Cuarto Test Unitario: Muere por solitario por 1 celula vida vecina

--- UnitTestKataGame/UnitTest.cs
+++ UnitTestKataGame/UnitTest.cs
@@ -72,6 +72,29 @@ namespace UnitTestKataGame
             Assert.AreEqual(expected, result);
         }
 
-       
+
+        [TestMethod]
+        public void DeadSolitaryOneCellLive()
+        {
+            Game game = new Game(5, 5);
+
+            game.Init(new List<KeyValuePair<int, int>>() {
+                new KeyValuePair<int, int>(2, 2),
+                new KeyValuePair<int, int>(3, 3),
+                new KeyValuePair<int, int>(4, 3),
+                new KeyValuePair<int, int>(3, 4),
+                new KeyValuePair<int, int>(4, 2)
+            });
+
+            bool expected = false;
+
+            game.Play();
+
+            bool? result = game.Dashboard[2, 2];
+
+            Assert.AreEqual(expected, result);
+        }
+
+
     }
 }
