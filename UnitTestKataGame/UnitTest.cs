8e99db1d133eb3d6d2aa06de7d0cc8a727e1b043
Deyvi Josue Ortiz Vidaurre <DORTIZV@farmacorp.com>
2019-08-28 22:18:57 -0400

    Segundo Test Unitario: Mantener Vivo cuando existe tres celulas vivas vecinas

--- UnitTestKataGame/UnitTest.cs
+++ UnitTestKataGame/UnitTest.cs
@@ -28,5 +28,26 @@ namespace UnitTestKataGame
 
             Assert.AreEqual(expected, result);
         }
+
+        [TestMethod]
+        public void KeepaLiveCaseThreeCellLives()
+        {
+            Game game = new Game(5, 5);
+
+            game.Init(new List<KeyValuePair<int, int>>() {
+                new KeyValuePair<int, int>(2, 2),
+                new KeyValuePair<int, int>(3, 3),
+                new KeyValuePair<int, int>(4, 3),
+                new KeyValuePair<int, int>(3, 4)
+            });
+
+            bool expected = true;
+
+            game.Play();
+
+            bool? result = game.Dashboard[3, 3];
+
+            Assert.AreEqual(expected, result);
+        }
     }
 }
